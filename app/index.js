ab1272d272ddf15c23ec7dc7d8d948c64906f754
Lucian Samson <lucian.samson@cynny.com>
2015-10-15 12:12:11 +0300

    loaded fragment

--- app/index.js
+++ app/index.js
@@ -1,32 +1,65 @@
 
+var width = window.innerWidth;
+var height = window.innerHeight;
 var component = require('./component');
 var app = document.createElement('div');
 var body = document.body;
-const PIXI = require('pixi.js');
-const renderer = new PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-const container = new PIXI.Container();
-const sprite = new PIXI.Sprite.fromImage(require('./assets/bean.jpg'));
+var PIXI = require('pixi.js');
+var renderer = new PIXI.autoDetectRenderer(width, height);
+var container = new PIXI.Container();
+var spriteBg = new PIXI.Sprite.fromImage(require('./assets/bg.jpg'));
+var sprite = new PIXI.Sprite.fromImage(require('./assets/bean.jpg'));
+var vert = require('./vert.glsl');
+var frag = require('./frag.glsl');
+var uniforms = {
+    resolution: {
+        type: '2f',
+        value: {
+            x: width,
+            y: height
+        }
+    },
+    time: {
+        type: '1f',
+        value: 0
+    }
+};
+var count = 0;
+var filter;
 
 // body.style.overflow = 'hidden';
 // app.appendChild(component());
 body.appendChild(renderer.view);
-window.requestAnimationFrame(animate);
+
 
 function loadProgressHandler(loader, resource) {
   console.log('loading: ' + resource.url + ' progress: ' + loader.progress + '%');
 }
 
-
 function setupAnimate() {
+    spriteBg.anchor.set(0.5);
+    spriteBg.width = width;
+    // spriteBg.height = height;
+    spriteBg.position.set(renderer.width / 2, renderer.height / 2);
+    console.log(frag);
+    filter = new PIXI.AbstractFilter(null, frag, uniforms);
+    // sprite.shader = filter;
+    spriteBg.filters = [filter];
+    container.addChild(spriteBg);
+
     sprite.anchor.set(0.5);
     sprite.position.set(renderer.width / 2, renderer.height / 2);
     sprite.scale.set(0.5);
-    container.addChild(sprite);
+    sprite.blendMode = PIXI.BLEND_MODES.ADD;
+    // container.addChild(sprite);
+
 }
+window.requestAnimationFrame(animate);
 
 function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(container);
+
+    renderer.render(container);
+    window.requestAnimationFrame(animate);
 }
 
 PIXI
--- webpack.config.js
+++ webpack.config.js
@@ -3,65 +3,68 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [
+    entry: [
     './app/some.less',
     './app/index'
-  ],
-  output: {
+    ],
+    output: {
     path: 'build',
     filename: 'bundle.js'
-  },
-  devServer: {
+    },
+    devServer: {
     historyApiFallback: true,
     hot: true,
     inline: true,
     progress: true,
     contentBase: './build/assets/'
-  },
-  plugins: [
+    },
+    plugins: [
     new HtmlWebpackPlugin({
       title: '@play',
       filename: 'index.html'
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel'
-        },
-        {
-            test: /\.json$/,
-            loader: 'json'
-        },
-        {
-            test: /\.less$/,
-            loader: "style!css!autoprefixer!less"
-        },
-        {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            loader: 'file?limit=10000!img?progressive=true'
-        },
-        {
-            test: /\.glsl$/,
-            loader: 'shader'
-        },
     ],
-    postLoaders: [
-      {
-          loader: "transform-loader?brfs",
-          include: path.resolve('/node_modules/pixi.js')
-      }
-    ],
-  },
-  resolve: {
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!autoprefixer!less"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'file?limit=10000!img?progressive=true'
+            },
+            {
+                test: /\.glsl$/,
+                loader: 'shader'
+            },
+        ],
+        postLoaders: [
+            {
+                loader: "transform-loader?brfs",
+                include: path.resolve('/node_modules/pixi.js')
+            }
+        ],
+    },
+    resolve: {
     // you can now require('file') instead of require('file.js')
-    extensions: ['', '.js', '.json', 'less']
-  },
-  node: {
-    fs: "empty"
-  }
+    extensions: ['', '.js', '.json', 'less', 'glsl']
+    },
+    node: {
+        fs: "empty"
+    },
+    glsl: {
+        chunkPath: 'chunks'
+    }
 };
